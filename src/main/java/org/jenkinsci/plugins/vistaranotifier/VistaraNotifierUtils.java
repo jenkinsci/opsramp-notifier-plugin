5a5ce39ea703835782299eacd844863cf28dc232
srinut096 <srinut096@gmail.com>
2016-02-25 17:32:03 +0530

    Alert description preparation modified

--- src/main/java/org/jenkinsci/plugins/vistaranotifier/VistaraNotifierUtils.java
+++ src/main/java/org/jenkinsci/plugins/vistaranotifier/VistaraNotifierUtils.java
@@ -30,7 +30,7 @@ public class VistaraNotifierUtils implements VistaraNotifierConstants {
 	 * @param state
 	 * @return
 	 */
-	public static Alert prepareVistaraAlert(final AbstractBuild<?, ?> build, final String state) {
+	public static Alert prepareVistaraAlert(AbstractBuild<?, ?> build, String state) {
     	Alert vistaraAlert = new Alert();
     	
     	vistaraAlert.setSubject(prepareAlertSubject(build, state));
@@ -51,7 +51,7 @@ public class VistaraNotifierUtils implements VistaraNotifierConstants {
 	 * @param state
 	 * @return
 	 */
-	public static String prepareAlertSubject(final AbstractBuild<?, ?> build, final String state) {
+	public static String prepareAlertSubject(AbstractBuild<?, ?> build, String state) {
     	String subject = build.getFullDisplayName() + SPACE;
         if(state.equalsIgnoreCase(FAILED)) {
         	subject += build.getResult();
@@ -72,82 +72,79 @@ public class VistaraNotifierUtils implements VistaraNotifierConstants {
 	 * @param subject
 	 * @return
 	 */
-	public static String prepareAlertDescription(final AbstractBuild<?, ?> build, final String state) {
+	public static String prepareAlertDescription(AbstractBuild<?, ?> build, String state) {
     	final AbstractBuild<?, ?> rootBuild = build.getRootBuild();
     	
     	StringBuffer description = new StringBuffer(build.getBuildStatusSummary().message + NEW_LINE);
-    	switch(state) {
-	    	case STARTED:
-		    	//Build triggered by
-		        List<Cause> causes = build.getCauses();
-		        String buildUser = null;
-		        if (causes.size() > 0 && (causes.get(0) instanceof Cause.UserIdCause)){
-		        	buildUser =  ((UserIdCause)causes.get(0)).getUserName();
-		        } else {
-		        	buildUser = ANONYMOUS_USER;
-		        }
-		        
-		        //Change log details
-		        StringBuffer changeLog = new StringBuffer(EMPTY_STR);
-		        StringBuffer summary = new StringBuffer(EMPTY_STR);
-		        ChangeLogSet<? extends ChangeLogSet.Entry> changeSet = rootBuild.getChangeSet();
-		        List<ChangeLogSet.Entry> entries = new LinkedList<ChangeLogSet.Entry>();
-		        for(Object o : changeSet.getItems()) {
-		        	ChangeLogSet.Entry entry = (ChangeLogSet.Entry) o;
-		            entries.add(entry);
-		        }
-		        
-		        if(entries.isEmpty()) {
-		            if(build.getDescription() != null && !build.getDescription().equals(EMPTY_STR)) {
-		            	changeLog.append(build.getDescription());
-		            } else { 
-		             	changeLog.append(causes.get(0).getShortDescription());
-		            }
-		        } else {
-		            for(ChangeLogSet.Entry entry : entries) {
-		            	summary.append(entry.getCommitId() + COLON + entry.getMsg() + NEW_LINE);
-		                changeLog.append(REV_STR).append(SPACE).append(BY_STR).append(SPACE)
-		                .append(entry.getAuthor().getDisplayName()).append(COLON).append(NEW_LINE)
-		                .append(entry.getCommitId()).append(COLON).append(entry.getMsg()).append(NEW_LINE);
-		                
-		                if(entry.getAffectedFiles() != null && entry.getAffectedFiles().size() > 0) {
-			                Iterator<?extends AffectedFile> files = entry.getAffectedFiles().iterator();
-			                changeLog.append(FILE_PATH).append(COLON).append(SPACE);
-			                while(files.hasNext()) {
-			                	AffectedFile file = files.next();
-			                	changeLog.append(file.getPath());
-			                }
+    	if(state.equals(STARTED)) {
+	    	//Build triggered by
+	        List<Cause> causes = build.getCauses();
+	        String buildUser = null;
+	        if (causes.size() > 0 && (causes.get(0) instanceof Cause.UserIdCause)){
+	        	buildUser =  ((UserIdCause)causes.get(0)).getUserName();
+	        } else {
+	        	buildUser = ANONYMOUS_USER;
+	        }
+	        
+	        //Change log details
+	        StringBuffer changeLog = new StringBuffer(EMPTY_STR);
+	        StringBuffer summary = new StringBuffer(EMPTY_STR);
+	        ChangeLogSet<? extends ChangeLogSet.Entry> changeSet = rootBuild.getChangeSet();
+	        List<ChangeLogSet.Entry> entries = new LinkedList<ChangeLogSet.Entry>();
+	        for(Object o : changeSet.getItems()) {
+	        	ChangeLogSet.Entry entry = (ChangeLogSet.Entry) o;
+	            entries.add(entry);
+	        }
+	        
+	        if(entries.isEmpty()) {
+	            if(build.getDescription() != null && !build.getDescription().equals(EMPTY_STR)) {
+	            	changeLog.append(build.getDescription());
+	            } else { 
+	             	changeLog.append(causes.get(0).getShortDescription());
+	            }
+	        } else {
+	            for(ChangeLogSet.Entry entry : entries) {
+	            	summary.append(entry.getCommitId() + COLON + entry.getMsg() + NEW_LINE);
+	                changeLog.append(REV_STR).append(SPACE).append(BY_STR).append(SPACE)
+	                .append(entry.getAuthor().getDisplayName()).append(COLON).append(NEW_LINE)
+	                .append(entry.getCommitId()).append(COLON).append(entry.getMsg()).append(NEW_LINE);
+	                
+	                if(entry.getAffectedFiles() != null && entry.getAffectedFiles().size() > 0) {
+		                Iterator<?extends AffectedFile> files = entry.getAffectedFiles().iterator();
+		                changeLog.append(FILE_PATH).append(COLON).append(SPACE);
+		                while(files.hasNext()) {
+		                	AffectedFile file = files.next();
+		                	changeLog.append(file.getPath());
 		                }
-		                changeLog.append(NEW_LINE).append(NEW_LINE);
-		            }
-		        }
-		         
-		        description.append(DESC_BUILD_USER + COLON + SPACE + buildUser + NEW_LINE);
-		        if(!summary.toString().equals(EMPTY_STR)) {
-		        	description.append(DESC_SUMMARY).append(COLON).append(NEW_LINE).append(summary.toString()).append(NEW_LINE);
-		        }
-		        description.append(DESC_CHANGE_LOG).append(COLON).append(NEW_LINE).append(changeLog.toString())
-		        .append(BUILD_NAME).append(COLON).append(SPACE).append(build.getProject().getName()).append(NEW_LINE)
-		        .append(BUILD_NUMBER).append(COLON).append(SPACE + build.getNumber()).append(NEW_LINE)
-		        .append(BUILD_URL).append(COLON).append(SPACE).append(build.getUrl()).append(NEW_LINE)
-		        .append(BUILD_FULL_URL).append(COLON).append(SPACE).append(Jenkins.getInstance().getRootUrl()).append(build.getUrl()).append(NEW_LINE);
-		        
-		        break;
-	    	case FAILED:// Send console output in case of build failure
-	    		try {
-		    		List<String> logLines = build.getLog(MAX_LINES);
-		    		if(logLines != null && !logLines.isEmpty()) {
-		    			description.append(CONSOLE_LOG_MSG1).append(SPACE).append(MAX_LINES).append(SPACE)
-		    			.append(CONSOLE_LOG_MSG2).append(COLON).append(NEW_LINE);
-		    			for(String log : logLines) {
-		    				description.append(log).append(NEW_LINE);
-		    			}
-		    		}
-	    		} catch(Exception e) {
-	    			//Ignore
+	                }
+	                changeLog.append(NEW_LINE).append(NEW_LINE);
+	            }
+	        }
+	         
+	        description.append(DESC_BUILD_USER + COLON + SPACE + buildUser + NEW_LINE);
+	        if(!summary.toString().equals(EMPTY_STR)) {
+	        	description.append(DESC_SUMMARY).append(COLON).append(NEW_LINE).append(summary.toString()).append(NEW_LINE);
+	        }
+	        
+	        description.append(DESC_CHANGE_LOG).append(COLON).append(NEW_LINE).append(changeLog.toString())
+	        .append(BUILD_NAME).append(COLON).append(SPACE).append(build.getProject().getName()).append(NEW_LINE)
+	        .append(BUILD_NUMBER).append(COLON).append(SPACE + build.getNumber()).append(NEW_LINE)
+	        .append(BUILD_URL).append(COLON).append(SPACE).append(build.getUrl()).append(NEW_LINE)
+	        .append(BUILD_FULL_URL).append(COLON).append(SPACE).append(Jenkins.getInstance().getRootUrl()).append(build.getUrl()).append(NEW_LINE);
+    	} else if(state.equals(FAILED)) {
+    		try {
+	    		List<String> logLines = build.getLog(MAX_LINES);
+	    		if(logLines != null && !logLines.isEmpty()) {
+	    			description.append(CONSOLE_LOG_MSG1).append(SPACE).append(MAX_LINES).append(SPACE)
+	    			.append(CONSOLE_LOG_MSG2).append(COLON).append(NEW_LINE);
+	    			for(String log : logLines) {
+	    				description.append(log).append(NEW_LINE);
+	    			}
 	    		}
-	    		break;
-	    	}
+    		} catch(Exception e) {
+    			//Ignore
+    		}
+	    }
     	
         return description.toString();
     }
@@ -156,7 +153,7 @@ public class VistaraNotifierUtils implements VistaraNotifierConstants {
 	 * @param build
 	 * @return
 	 */
-	public static Device prepareHost(final AbstractBuild<?, ?> build) {
+	public static Device prepareHost(AbstractBuild<?, ?> build) {
         String nodeName = build.getBuiltOn().getNodeName();
         if (!build.getProject().getName().equals(build.getRootBuild().getProject().getName())) {
             nodeName = build.getProject().getName();  //label
@@ -175,7 +172,7 @@ public class VistaraNotifierUtils implements VistaraNotifierConstants {
 	 * @param state
 	 * @return
 	 */
-	public static String prepareAlertState(final String state) {
+	public static String prepareAlertState(String state) {
 		if(state == null || state.equals(EMPTY_STR)) {
 			return APIConstants.INFO;
 		}
@@ -197,7 +194,7 @@ public class VistaraNotifierUtils implements VistaraNotifierConstants {
      * @return
      */
     @SuppressWarnings("unused")
-	private String prepareVistaraAlertJson(final AbstractBuild<?, ?> build, final String state) {
+	private String prepareVistaraAlertJson(AbstractBuild<?, ?> build, String state) {
     	final AbstractBuild<?, ?> rootBuild = build.getRootBuild();
         JSONObject json = new JSONObject();
         
